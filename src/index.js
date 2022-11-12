7bf7ff57003fe38aa31483db66dda675eccf08b1
zhiwei <mac@zhiwei.local>
2022-11-12 14:14:49 -0500

    finish prevBtn, nextBtn and dots

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from "./App";
 
-const myFirstElement=<h1>Hello World</h1>
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  myFirstElement
+    <App/>
 );
 
 // If you want to start measuring performance in your app, pass a function
